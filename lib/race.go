653d3d18e9d3b09c9923d2c91b2e1db33e410b54
Matthias Küch <mail@matthias-kuech.de>
2024-02-23 09:30:18 +0100

    Add workflow

---
+++ lib/race.go
@@ -0,0 +1,59 @@
+package lib
+
+import (
+	"fmt"
+	gt7 "github.com/snipem/go-gt7-telemetry/lib"
+	"time"
+)
+
+func logGt7(c *gt7.GT7Communication, gt7stats *Stats) {
+	for {
+
+		if c.LastData.CurrentLap == 0 {
+			// Race reset
+
+			gt7stats.fuelConsumptionLastLap = 0
+			time.Sleep(100 * time.Millisecond)
+			continue
+		}
+
+		if len(gt7stats.Laps) == 0 {
+			gt7stats.Laps = append(gt7stats.Laps, Lap{
+				FuelStart: c.LastData.CurrentFuel,
+				Number:    c.LastData.CurrentLap,
+			})
+		}
+
+		if gt7stats.Laps[len(gt7stats.Laps)-1].Number != c.LastData.CurrentLap {
+			// Change of laps detected
+
+			if c.LastData.CurrentLap == 1 {
+				// First crossing of the line
+				gt7stats.raceStartTime = time.Now()
+				gt7stats.fuelConsumptionLastLap = 0
+				fmt.Printf("RACE START 🏁 %s \n", gt7stats.raceStartTime.Format("2006-01-02 15:04:05"))
+			}
+
+			gt7stats.Laps[len(gt7stats.Laps)-1].FuelEnd = c.LastData.CurrentFuel
+			gt7stats.Laps[len(gt7stats.Laps)-1].Duration = GetDurationFromGT7Time(c.LastData.LastLap)
+
+			// Do not log last laps fuel consumption in the first lap
+			if c.LastData.CurrentLap != 1 {
+				gt7stats.fuelConsumptionLastLap = gt7stats.Laps[len(gt7stats.Laps)-1].FuelStart - gt7stats.Laps[len(gt7stats.Laps)-1].FuelEnd
+				gt7stats.Laps[len(gt7stats.Laps)-1].FuelConsumed = gt7stats.fuelConsumptionLastLap
+			}
+
+			fmt.Printf("Add new Lap. Last Lap was: %s\n", gt7stats.Laps[len(gt7stats.Laps)-1])
+
+			newLap := Lap{
+				FuelStart: c.LastData.CurrentFuel,
+				Number:    c.LastData.CurrentLap,
+			}
+			gt7stats.Laps = append(gt7stats.Laps, newLap)
+
+		}
+		// FIXME Use deep copy here
+		gt7stats.LastLoggedData.FuelCapacity = c.LastData.FuelCapacity
+		time.Sleep(100 * time.Millisecond)
+	}
+}
